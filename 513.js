e5cb2b4114797645f8752b53bd3ba9c2056ea6a8
Mariusz Kujawski <email@mariuszkujawski.pl>
2015-04-28 18:39:45 +0200

    Problem #513 update

--- 513.js
+++ 513.js
@@ -1,9 +1,9 @@
 // not finished
 function f(n) {
 	var count = 0;
-	for (var c = 3; c <= n; c++) {
+	for (var c = 4; c <= n; c+=2) {
 		console.log(c);
-	for (var b = 2; b <= c; b++) {
+	for (var b = 3; b <= c; b++) {
 	for (var a = c-b; a <= b; a++) {
 		if (a+b>c && b+c>a) {
 			var m = Math.sqrt((2*b*b + 2*a*a - c*c)/4);
